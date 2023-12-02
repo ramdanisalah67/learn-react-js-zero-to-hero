38cb7753d5b678c776016209684dbeac94dcc492
ramdanisalah67 <ramdanisalah67@gmail.com>
2023-12-03 00:33:47 +0100

    react js basic component,declare varibale ... part 1

--- src/App.js
+++ src/App.js
@@ -2,23 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  //declare variable
+  let name = "ramdani salah-eddine";
+  let note =18
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+   <h2>welcome {name} to your react app</h2>
+    {/*Conditions */}
+    {note>10? <p>Admis</p> : <p>Non Admis</p>}
+   <img src="logo192.png" alt="" />
+   </div>
   );
 }
 
